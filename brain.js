8cb7c9c0f04a4122e56512ab9dd07e52d8244200
Sparsh <68753589+i-sparsh-sharma@users.noreply.github.com>
2022-07-16 05:46:47 +0530

    Add files via upload

---
+++ brain.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/5ffc35c0c31c9117cb6d6b34/1eroj3uke';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
